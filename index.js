2278e0069f70afd36331d7788a3d46e18f734c5e
jon <jonathan.brandwein@gmail.com>
2018-06-11 16:37:38 -0400

    port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 app.get('/emea', (req, res) => {
     res.render('emea')
 })
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
