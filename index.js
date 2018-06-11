2d4ea7793b1a1250d0bad8bdd2b38b4b12bdef98
jon <jonathan.brandwein@gmail.com>
2018-06-11 16:34:18 -0400

    changed port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 app.get('/emea', (req, res) => {
     res.render('emea')
 })
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
